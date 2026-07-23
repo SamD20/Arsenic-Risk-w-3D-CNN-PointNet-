326d7b3880b79ac0fb98b285d48d798bcf49e6e5
Sam <up2247116@myport.ac.uk>
2026-07-23 15:50:39 +0100

    Switched to random sampling over balanced to better represent dataset

--- scripts/dataloader.py
+++ scripts/dataloader.py
@@ -10,47 +10,7 @@ EPOCH_SIZE = 300000 #wells per epoch
 VALIDATION_SIZE = 300000 #wells per validation
 RISK_CLASSES = [10,50]
 # <=10 = Low Risk, 10 to <=50 = Medium Risk, >50 = High Risk
-
-class BalancedRiskSampler(Sampler):
-    def __init__(self, dataset, epoch_size):
-        arsenic = dataset.Arsenic
-
-        LOW = RISK_CLASSES[0]
-        HIGH = RISK_CLASSES[1]
-
-        self.low = np.where(arsenic <= LOW)[0]
-        self.medium = np.where((arsenic > LOW) & (arsenic <= HIGH))[0]
-        self.high = np.where(arsenic > HIGH)[0]
-
-        self.samples_per_class = epoch_size // 3
-        self.length = self.samples_per_class * 3
-
-    def __iter__(self):
-        low = np.random.choice(
-            self.low,
-            self.length // 3,
-            replace=False
-        )
-
-        medium = np.random.choice(
-            self.medium,
-            self.length // 3,
-            replace=False
-        )
-
-        high = np.random.choice(
-            self.high,
-            self.length // 3,
-            replace=False
-        )
-
-        indexes = np.concatenate([low,medium,high])
-
-        np.random.shuffle(indexes)
-        return iter(indexes.tolist())
-
-    def __len__(self):
-        return self.length
+rng = np.random.default_rng(RANDOM_SEED)
 
 def collate_fn(batch):
 
@@ -67,7 +27,8 @@ def collate_fn(batch):
     }
 
 def get_dataloader(dataset,batch_size=BATCH_SIZE,workers=NUM_WORKERS):
-    sampler = BalancedRiskSampler(dataset, EPOCH_SIZE)
+    val_indexes = rng.choice(len(dataset),VALIDATION_SIZE,replace=False)
+    sampler = SubsetRandomSampler(val_indexes)
 
     loader = DataLoader(
         dataset,
@@ -83,7 +44,6 @@ def get_dataloader(dataset,batch_size=BATCH_SIZE,workers=NUM_WORKERS):
     return loader
 
 def get_validation_dataloader(dataset, batch_size=BATCH_SIZE, workers=NUM_WORKERS):
-    rng = np.random.default_rng(RANDOM_SEED)
     val_indexes = rng.choice(len(dataset),VALIDATION_SIZE,replace=False)
     sampler = SubsetRandomSampler(val_indexes)
 
